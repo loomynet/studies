e464e6f6467fb2791358c0099bcd3bbcf8a94c22
Andrejs Jakuņins <95055364+DucksSupplier@users.noreply.github.com>
2024-06-22 02:05:07 +0300

    Add files via upload

---
+++ 4fun/lines.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int symbolCount;
+	char symbol;
+	int lineType;
+
+	cout << "This program makes vartical or horizontal lines." << endl;
+	cout << "Input symbol count: ";
+	cin >> symbolCount;
+	cout << endl << "Choose the symbol: ";
+	cin >> symbol;
+	cout << endl << "Choose the line type:" << endl <<
+					"1 - horizontal line"   << endl <<
+					"2 - vertical line"     << endl;
+	cin >> lineType;
+
+	if(lineType==1)
+	{
+		while (symbolCount > 0)
+		{
+			cout << symbol;
+			symbolCount--;
+		}
+	}
+	
+	else if (lineType==2)
+	{
+		while (symbolCount > 0)
+		{
+			cout << symbol << endl;
+			symbolCount--;
+		}
+	}
+	
+	else
+	{ 
+		cout << "Error! The line type identifier is incorrect! Restart the program." << endl;
+	}
+
+	return 0;
+}
