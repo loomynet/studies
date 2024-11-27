c29485a2144fdbc2d29104638efe5d71c2b97119
Andrejs Jakunins <duckssupplier@gmail.com>
2024-11-27 13:04:43 +0200

    jar

--- oop/TSI/Lab1.java
+++ oop/TSI/Lab1.java
@@ -1,95 +1,129 @@
-public class Lab1 {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class Lab1 {
     public static class Complex {
         private double real;
         private double imaginary;
 
-        //Constructor
+        // Constructor
         public Complex(double real, double imaginary) {
             this.real = real;
             this.imaginary = imaginary;
         }
 
         // Addition
-        public Complex Add(Complex other) {
-            return new Complex(this.real + other.real, this.imaginary + other.imaginary);
+        public Complex add(double real, double imaginary) {
+            return new Complex(this.real + real, this.imaginary + imaginary);
         }
 
         // Subtraction
-        public Complex Sub(Complex other) {
-            return new Complex(this.real - other.real, this.imaginary - other.imaginary);
+        public Complex sub(double real, double imaginary) {
+            return new Complex(this.real - real, this.imaginary - imaginary);
         }
 
         // Multiplication
-        public Complex Mul(Complex other) {
-            double realPart = (this.real * other.real) - (this.imaginary * other.imaginary);
-            double imaginaryPart = (this.real * other.imaginary) + (this.imaginary * other.real);
+        public Complex mul(double real, double imaginary) {
+            double realPart = (this.real * real) - (this.imaginary * imaginary);
+            double imaginaryPart = (this.real * imaginary) + (this.imaginary * real);
             return new Complex(realPart, imaginaryPart);
         }
 
         // Division
-        public Complex Div(Complex other) throws ArithmeticException {
-            double denominator = (other.real * other.real) + (other.imaginary * other.imaginary);
+        public Complex div(double real, double imaginary) throws ArithmeticException {
+            double denominator = (real * real) + (imaginary * imaginary);
             if (denominator == 0) {
                 throw new ArithmeticException("Cannot divide by a complex number with zero magnitude.");
             }
 
-            double realPart = (this.real * other.real + this.imaginary * other.imaginary) / denominator;
-            double imaginaryPart = (this.imaginary * other.real - this.real * other.imaginary) / denominator;
+            double realPart = (this.real * real + this.imaginary * imaginary) / denominator;
+            double imaginaryPart = (this.imaginary * real - this.real * imaginary) / denominator;
             return new Complex(realPart, imaginaryPart);
         }
 
-        // Comparison
-        public boolean Equals(Complex other) {
-            return (this.real == other.real) && (this.imaginary == other.imaginary);
-        }
-
         // Conjugate
-        public Complex Conjugate() {
+        public Complex conjugate() {
             return new Complex(this.real, -this.imaginary);
         }
 
+        // Comparison
+        public boolean compare(Complex other) {
+            return (this.real == other.real) && (this.imaginary == other.imaginary);
+        }
+
         // Display method
-        public void Display() {
+        public void display() {
             System.out.println(real + " + " + imaginary + "i");
         }
 
-    }
-    public static void main(String[] args) {
-        Complex c1 = new Complex(3, 4);
-        Complex c2 = new Complex(1, 2);
+        public void setReal(double real) {
+            this.real = real;
+        }
 
-        System.out.println("Complex Number 1:");
-        c1.Display();
-        System.out.println("Complex Number 2:");
-        c2.Display();
+        public void setImaginary(double imaginary) {
+            this.imaginary = imaginary;
+        }
+
+        public double getReal() {
+            return this.real;
+        }
+
+        public double getImaginary() {
+            return this.imaginary;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Author: Andrejs Jakunins");
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        System.out.println("Date and time: " + timeStamp);
+
+        Complex complex = new Complex(3, 4);
+        Complex complexCompare = new Complex(3, 5);
+
+        System.out.println("\nComplex Number: ");
+        complex.display();
 
         System.out.println("\nAddition:");
-        Complex sum = c1.Add(c2);
-        sum.Display();
+        Complex sum = complex.add(1,1);
+        sum.display();
 
         System.out.println("\nSubtraction:");
-        Complex difference = c1.Sub(c2);
-        difference.Display();
+        Complex difference = complex.sub(3, 5);
+        difference.display();
 
         System.out.println("\nMultiplication:");
-        Complex product = c1.Mul(c2);
-        product.Display();
+        Complex product = complex.mul(2, 9);
+        product.display();
 
         System.out.println("\nDivision:");
         try {
-            Complex quotient = c1.Div(c2);
-            quotient.Display();
-        }
-        catch (ArithmeticException e) {
+            Complex quotient = complex.div(0, 0);
+            quotient.display();
+        } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("\nConjugate of Complex Number 1:");
-        Complex conjugate = c1.Conjugate();
-        conjugate.Display();
+        System.out.println("\nConjugate of Complex Number:");
+        Complex conjugate = complex.conjugate();
+        conjugate.display();
 
         System.out.println("\nAre Complex Number 1 and Complex Number 2 equal?");
-        System.out.println(c1.Equals(c2) ? "Yes" : "No");
+        System.out.println(complex.compare(complexCompare) ? "Yes" : "No");
+
+        System.out.println("\nSet the real number to 1: ");
+        complex.setReal(1);
+        complex.display();
+
+        System.out.println("\nSet the imaginary number to 1: ");
+        complex.setImaginary(1);
+        complex.display();
+
+        System.out.println("\nGet the real number: ");
+        System.out.println(complex.getReal());
+
+        System.out.println("\nGet the imaginary number: ");
+        System.out.println(complex.getImaginary());
     }
 }
