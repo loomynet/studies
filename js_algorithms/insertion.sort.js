1dbf9523628654f5de90bd78429292ca0ef7fce3
Andrejs Jakunins <duckssupplier@gmail.com>
2024-12-04 10:27:54 +0200

    Add insertion sort algorithm implementation in JavaScript

---
+++ js_algorithms/insertion.sort.js
@@ -0,0 +1,17 @@
+array = [50,45,60,85,90,33,1,21,67]
+
+function insertionSort(array) {
+    for (i = 1; i < array.length; i++) {
+        temp = array[i];
+        j = i - 1;
+
+        while (j >= 0 && array[j] > temp) {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = temp;
+    }
+    return array;
+}
+
+console.log(insertionSort(array));
