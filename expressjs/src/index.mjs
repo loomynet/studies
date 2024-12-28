d5456f84a5c26dfc48ea8ae4ae56692eba921e2b
Andrejs Jakunins <duckssupplier@gmail.com>
2024-12-28 10:01:18 +0200

    feat: add PUT endpoint for updating user information in Express.js API

--- expressjs/src/index.mjs
+++ expressjs/src/index.mjs
@@ -59,3 +59,19 @@ app.get("/api/users/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
 });
+
+app.put("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return res.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+  if (findUserIndex === -1) return res.statusCode(404);
+  mockUsers[findUserIndex] = { id: parsedId, ...body };
+  return res.sendStatus(200);
+});
