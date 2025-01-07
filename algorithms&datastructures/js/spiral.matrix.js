6f29d4ecc2a7c93736895ab258a99f7d352c3f58
Andrejs Jakuņins <95055364+loomynet@users.noreply.github.com>
2025-01-07 08:30:27 +0200

    Update spiral.matrix.js
    Finished but not tested code

--- algorithms&datastructures/js/spiral.matrix.js
+++ algorithms&datastructures/js/spiral.matrix.js
@@ -7,19 +7,26 @@ var spiralOrder = function (matrix) {
   let n = matrix[0].length; //w
   let arr = [];
   let top = 0;
-  let bottom = m;
-  let left = n;
-  let right = n;
-  while (true) {
-    for (let i = 0; i < right; i++) {
-      arr.push(matrix[0][i]);
+  let bottom = m-1;
+  let left = 0;
+  let right = n-1;
+  while (bottom != top || left != right) {
+    for (let i = 0; i <= right; i++) {
+      arr.push(matrix[top][i]);
     }
     top++;
-    for (let i = 1; i < bottom; i++) {
-      arr.push(matrix[i][right - 1]);
+    for (let i = top; i <= bottom; i++) {
+      arr.push(matrix[i][right]);
     }
     right--;
-    for (let i = 0; i < array.length; i++) {}
+    for (let i = right; i >= right - left; i--) {
+      arr.push(matrix[bottom][i]);
+    }
+    bottom--;
+    for (let i = bottom; i > top; i--) {
+      arr.push(matrix[0][i]);
+    }
+    left++;
   }
 
   return arr;
