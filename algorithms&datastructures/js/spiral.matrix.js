0240cc63432a837d8846168ab4296b21e01831b6
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-07 12:40:32 +0200

    fix: correct spiral matrix traversal logic and improve boundary checks

--- algorithms&datastructures/js/spiral.matrix.js
+++ algorithms&datastructures/js/spiral.matrix.js
@@ -7,26 +7,33 @@ var spiralOrder = function (matrix) {
   let n = matrix[0].length; //w
   let arr = [];
   let top = 0;
-  let bottom = m-1;
+  let bottom = m - 1;
   let left = 0;
-  let right = n-1;
-  while (bottom != top || left != right) {
-    for (let i = 0; i <= right; i++) {
+  let right = n - 1;
+  while (top <= bottom && left <= right) {
+    for (let i = left; i <= right; i++) {
       arr.push(matrix[top][i]);
     }
     top++;
+
     for (let i = top; i <= bottom; i++) {
       arr.push(matrix[i][right]);
     }
     right--;
-    for (let i = right; i >= right - left; i--) {
-      arr.push(matrix[bottom][i]);
+
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--) {
+        arr.push(matrix[bottom][i]);
+      }
+      bottom--;
     }
-    bottom--;
-    for (let i = bottom; i > top; i--) {
-      arr.push(matrix[0][i]);
+
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--) {
+        arr.push(matrix[i][left]);
+      }
+      left++;
     }
-    left++;
   }
 
   return arr;
@@ -38,8 +45,4 @@ matrix = [
   [9, 10, 11, 12],
 ];
 
-//
-// 1.[0,0] -> [0,n]
-// 2.[0,n] -> [m,n]
-// 3.[m,n] -> [m,0] ... shift, pop, laps++
-// 4.[m,0] -> [0,0]
+console.log(spiralOrder(matrix));
