e9dd60edae44786b227dfa11aa4ed5a571851cf6
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-09 08:09:31 +0200

    refactor: clean up select sort implementation for better readability

--- algorithms&datastructures/js/select.sort.js
+++ algorithms&datastructures/js/select.sort.js
@@ -1,26 +1,24 @@
-array = [60,75,80,50,90]
-
-var aboba;
+array = [60, 75, 80, 50, 90];
 
 function selectSort(array) {
-    len = array.length - 1;
-    var minIndex;
-    for (var i = 0 ; i < len; i++) {
-        minIndex = i;
-        var minValue = array[minIndex];
-        for (var j = i; j < len; j++) {
-            if (minValue > array[j + 1]) {
-                minValue = array[j + 1];
-                minIndex = j + 1;
-            }
-        }
-        if (i != minIndex){
-            var temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
-        }
+  len = array.length - 1;
+  var minIndex;
+  for (var i = 0; i < len; i++) {
+    minIndex = i;
+    var minValue = array[minIndex];
+    for (var j = i; j < len; j++) {
+      if (minValue > array[j + 1]) {
+        minValue = array[j + 1];
+        minIndex = j + 1;
+      }
     }
-    return array;
+    if (i != minIndex) {
+      var temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
+  }
+  return array;
 }
 
 console.log(selectSort(array));
