749dc38d38861fab1ea323b568fa2f528dbb7efb
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-13 00:55:30 +0200

    feat: simplify user retrieval endpoint by removing validation schema and enhancing query handling

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -53,27 +53,15 @@ app.get("/api", (req, res) => {
   res.status(200).send({ msg: "Hello" });
 });
 
-// TODO
-// Create validation schema for this method
-app.get(
-  "/api/users",
-  query("filter")
-    .isString()
-    .notEmpty()
-    .withMessage("Must not be empty")
-    .isLength({ min: 3, max: 10 })
-    .withMessage("Must be at least 3-10 characters"),
-  (req, res) => {
-    const result = validationResult(req);
-    console.log(result);
-    const {
-      query: { filter, value },
-    } = req;
-    if (filter && value)
-      return res.send(mockUsers.filter((user) => user[filter].includes(value)));
-    return res.send(mockUsers);
+app.get("/api/users", (req, res) => {
+  const {
+    query: { filter, value },
+  } = req;
+  if (filter && value) {
+    return res.send(mockUsers.filter((user) => user[filter].includes(value)));
   }
-);
+  return res.send(mockUsers);
+});
 
 app.post("/api/users", checkSchema(createUserValidationSchema), (req, res) => {
   const result = myValidationResult(req);
