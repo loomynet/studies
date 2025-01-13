7be5516928762b2d4c21c64b2cec3558df13a10d
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-13 18:24:49 +0200

    feat: enhance user retrieval endpoint with query validation for filter parameter

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -53,15 +53,24 @@ app.get("/api", (req, res) => {
   res.status(200).send({ msg: "Hello" });
 });
 
-app.get("/api/users", (req, res) => {
-  const {
-    query: { filter, value },
-  } = req;
-  if (filter && value) {
-    return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3-10 characters"),
+  (req, res) => {
+    const {
+      query: { filter, value },
+    } = req;
+    if (filter && value) {
+      return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+    }
+    return res.send(mockUsers);
   }
-  return res.send(mockUsers);
-});
+);
 
 app.post("/api/users", checkSchema(createUserValidationSchema), (req, res) => {
   const result = myValidationResult(req);
