5015f16da05f1b2fb2f22bf09f7de591616517f6
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-13 23:04:46 +0200

    feat: restructure user and product routes into separate files for better organization

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -1,49 +1,11 @@
 import { PORT } from "./config/config.js";
 import express from "express";
-import {
-  query,
-  validationResult,
-  body,
-  matchedData,
-  checkSchema,
-} from "express-validator";
-import { createUserValidationSchema } from "./utils/validationSchemas.js";
+import routes from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
-
-const myValidationResult = validationResult.withDefaults({
-  formatter: (error) => error.msg,
-});
-
-const loggingMiddleware = (req, res, next) => {
-  console.log(`${req.method} - ${req.url}`);
-  next();
-};
-
-const resolveIndexByUserId = (req, res, next) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) return res.sendStatus(400);
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) return res.sendStatus(404);
-  req.findUserIndex = findUserIndex;
-  next();
-};
-
-const mockUsers = [
-  { id: 1, username: "anson", displayName: "Anson" },
-  { id: 2, username: "jack", displayName: "Jack" },
-  { id: 3, username: "adam", displayName: "Adam" },
-  { id: 4, username: "linda", displayName: "Linda" },
-  { id: 5, username: "mike", displayName: "Mike" },
-  { id: 6, username: "matt", displayName: "Matt" },
-  { id: 7, username: "nick", displayName: "Nick" },
-];
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Listening at: http://localhost:${PORT}`);
@@ -51,64 +13,4 @@ app.listen(PORT, () => {
 
 app.get("/api", (req, res) => {
   res.status(200).send({ msg: "Hello" });
-});
-
-app.get(
-  "/api/users",
-  query("filter")
-    .isString()
-    .notEmpty()
-    .withMessage("Must not be empty")
-    .isLength({ min: 3, max: 10 })
-    .withMessage("Must be at least 3-10 characters"),
-  (req, res) => {
-    const {
-      query: { filter, value },
-    } = req;
-    if (filter && value) {
-      return res.send(mockUsers.filter((user) => user[filter].includes(value)));
-    }
-    return res.send(mockUsers);
-  }
-);
-
-app.post("/api/users", checkSchema(createUserValidationSchema), (req, res) => {
-  const result = myValidationResult(req);
-  if (!result.isEmpty()) {
-    return res.status(400).send({ errors: result.array() });
-  }
-
-  const data = matchedData(req);
-  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data };
-  mockUsers.push(newUser);
-  return res.status(201).send(newUser);
-});
-
-app.get("/api/users/:id", resolveIndexByUserId, (req, res) => {
-  const { findUserIndex } = req;
-  const findUser = mockUsers[findUserIndex];
-  if (!findUser) return res.sendStatus(404);
-  return res.send(findUser);
-});
-
-app.get("/api/products", (req, res) => {
-  res.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
-});
-
-app.put("/api/users/:id", resolveIndexByUserId, (req, res) => {
-  const { body, findUserIndex } = req;
-  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
-  return res.sendStatus(200);
-});
-
-app.patch("/api/users/:id", resolveIndexByUserId, (req, res) => {
-  const { body, findUserIndex } = req;
-  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
-  return res.sendStatus(200);
-});
-
-app.delete("/api/users/:id", resolveIndexByUserId, (req, res) => {
-  const { findUserIndex } = req;
-  mockUsers.splice(findUserIndex, 1);
-  return res.sendStatus(200);
 });
---
+++ expressjs/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const mockUsers = [
+  { id: 1, username: "anson", displayName: "Anson" },
+  { id: 2, username: "jack", displayName: "Jack" },
+  { id: 3, username: "adam", displayName: "Adam" },
+  { id: 4, username: "linda", displayName: "Linda" },
+  { id: 5, username: "mike", displayName: "Mike" },
+  { id: 6, username: "matt", displayName: "Matt" },
+  { id: 7, username: "nick", displayName: "Nick" },
+];
