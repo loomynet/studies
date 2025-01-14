dcac249f07a1e5c42ba5ce04ef0806cdaffcb0f1
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-14 08:51:12 +0200

    feat: add cookie-parser middleware and implement cookie-based access control for product route

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -1,10 +1,12 @@
 import { PORT } from "./config/config.js";
 import express from "express";
 import routes from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
 
 app.listen(PORT, () => {
@@ -12,5 +14,6 @@ app.listen(PORT, () => {
 });
 
 app.get("/api", (req, res) => {
+  res.cookie("hello", "world", { maxAge: 60000 * 60 * 2 });
   res.status(200).send({ msg: "Hello" });
 });
--- expressjs/src/routes/products.js
+++ expressjs/src/routes/products.js
@@ -3,7 +3,11 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
-  res.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
+  console.log(req.cookies);
+  if (req.cookies.hello && req.cookies.hello === "world") {
+    return res.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
+  }
+  return res.status(401).send({ msg: "Sorry, you need the correct cookie." });
 });
 
 export default router;
