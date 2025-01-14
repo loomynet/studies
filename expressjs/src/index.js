544042bdbe0cc811f1614e916e455ef0923d7f82
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-14 10:22:09 +0200

    feat: add express-session for user authentication and session management

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -1,12 +1,25 @@
 import { PORT } from "./config/config.js";
+import { secret } from "./config/config.js";
 import express from "express";
 import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import { mockUsers } from "./utils/constants.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    secret: secret,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
 app.use(routes);
 
 app.listen(PORT, () => {
@@ -16,4 +29,25 @@ app.listen(PORT, () => {
 app.get("/api", (req, res) => {
   res.cookie("hello", "world", { maxAge: 60000 * 60 * 2 });
   res.status(200).send({ msg: "Hello" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ msg: "Bad credentials" });
+
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not authenticated" });
 });
--- expressjs/src/utils/constants.js
+++ expressjs/src/utils/constants.js
@@ -1,9 +1,9 @@
 export const mockUsers = [
-  { id: 1, username: "anson", displayName: "Anson" },
-  { id: 2, username: "jack", displayName: "Jack" },
-  { id: 3, username: "adam", displayName: "Adam" },
-  { id: 4, username: "linda", displayName: "Linda" },
-  { id: 5, username: "mike", displayName: "Mike" },
-  { id: 6, username: "matt", displayName: "Matt" },
-  { id: 7, username: "nick", displayName: "Nick" },
+  { id: 1, username: "anson", displayName: "Anson", password: "password123" },
+  { id: 2, username: "jack", displayName: "Jack", password: "password124" },
+  { id: 3, username: "adam", displayName: "Adam", password: "password125" },
+  { id: 4, username: "linda", displayName: "Linda", password: "password126" },
+  { id: 5, username: "mike", displayName: "Mike", password: "password127" },
+  { id: 6, username: "matt", displayName: "Matt", password: "password128" },
+  { id: 7, username: "nick", displayName: "Nick", password: "password129" },
 ];
