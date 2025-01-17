05a0d1b32653768e403e153d4d15f9d89c87752d
Andrejs Jakunins <duckssupplier@gmail.com>
2025-01-17 18:06:21 +0200

    feat: implement PropertyContainer class for dynamic property management in Character class

---
+++ php/patterns/propertyContainer/index.php
@@ -0,0 +1,69 @@
+<?php
+
+class PropertyContainer {
+	protected $properties = [];
+
+	public function addProperty( $property, $value ) {
+		if ( isset( $this->properties[ $property ] ) ) {
+			throw new Exception( "Property '$property' already exists, to change the value, use updateProperty method." );
+		}
+		$this->properties[ $property ] = $value;
+	}
+
+	public function deleteProperty( $property ) {
+		if ( empty( $this->properties[ $property ] ) ) {
+			throw new Exception( "Property '$property' does not exist." );
+		}
+		unset( $this->properties[ $property ] );
+	}
+
+	public function getProperty( $property ) {
+		return $this->properties[ $property ] ?? null;
+	}
+
+	public function updateProperty( $property, $value ) {
+		if ( empty( $this->properties[ $property ] ) ) {
+			throw new Exception( "Property '$property' does not exist." );
+		}
+		$this->properties[ $property ] = $value;
+	}
+}
+
+class Character extends PropertyContainer {
+	private $name;
+	public function __construct( $name ) {
+		$this->name = $name;
+	}
+	public function displayName() {
+		echo "Character name: $this->name\n<br>";
+	}
+	public function displayProperties() {
+		foreach ( $this->properties as $property => $value ) {
+			echo "$property: $value \n<br>";
+		}
+	}
+
+	public function displayInfo() {
+		echo "Character name: $this->name\n<br>";
+		foreach ( $this->properties as $property => $value ) {
+			echo "$property: $value \n<br>";
+		}
+	}
+}
+
+$aboba = new Character( "Aboba" );
+$aboba->addProperty( "Health", 500 );
+$aboba->addProperty( "Mana", 2 );
+$aboba->addProperty( "Strength", 15 );
+$aboba->addProperty( "Agility", 3 );
+$aboba->addProperty( "Intelligence", -1 );
+
+$aboba->displayInfo();
+
+$aboba->updateProperty( "Health", 432 );
+$aboba->deleteProperty( "Intelligence" );
+
+$aboba->displayInfo();
+
+$abobahp = $aboba->getProperty( "Health" );
+echo "<br> Character aboba health: $abobahp HP <br>";
