45f2440ff93712cb948bffff2acfd026e7906843
Andrejs Jakunins <andrejsjakunins@gmail.com>
2025-02-20 16:26:37 +0200

    refactor: simplify print statements in event time calculation

--- PythonEssentials/part_1/event_time.py
+++ PythonEssentials/part_1/event_time.py
@@ -6,7 +6,5 @@ time = (hour*60) + mins + duration
 hour = time // 60 % 24
 mins = time % 60
 
-if(mins<10):
-    print(f"Event ends at: {hour}:0{mins}")
-else:
-    print(f"Event ends at: {hour}:{mins}")
+if(mins<10): print(f"Event ends at: {hour}:0{mins}")
+else: print(f"Event ends at: {hour}:{mins}")
