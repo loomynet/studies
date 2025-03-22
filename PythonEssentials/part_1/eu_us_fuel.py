b53b26ffb894e7240f86c798a13b020d3f9b1d73
Andrejs Jakunins <andrejsjakunins@gmail.com>
2025-03-22 18:25:19 +0200

    feat: add fuel efficiency conversion functions between liters/100km and miles/gallon

---
+++ PythonEssentials/part_1/eu_us_fuel.py
@@ -0,0 +1,18 @@
+mile_in_meters = 1609.344
+gallon_in_litres = 3.785411784
+
+
+def liters_100km_to_miles_gallon(liters):
+    return 235.215 / (liters)
+
+
+def miles_gallon_to_liters_100km(miles):
+    return 235.215 / (miles)
+
+
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
